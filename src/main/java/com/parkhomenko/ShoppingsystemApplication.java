57ceae02e72debb106ca906db65c3c34d2dd09d1
Dmytro <dimagit24@gmail.com>
2018-03-19 22:13:18 +0200

    P1: generated project with springboot cli tool

---
+++ src/main/java/com/parkhomenko/ShoppingsystemApplication.java
@@ -0,0 +1,12 @@
+package com.parkhomenko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingsystemApplication.class, args);
+	}
+}
